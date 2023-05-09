745d7bffb7110642befd3fdeeb98f54353bad8d3
Philippe Le Quoc <philippe.lequoc@gmail.com>
2023-05-09 17:26:15 +0200

    secure link

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
-const connectionString = `mongodb+srv://philippelacapsule:H1r0nd3773@cluster0.6dvqohn.mongodb.net/actorproject`;
+const connectionString = `mongodb+srv://${CONNECTION_STRING}@cluster0.6dvqohn.mongodb.net/actorproject`;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
